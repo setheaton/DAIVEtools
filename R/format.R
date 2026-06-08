2cade9b8cafcb77a7a5bf0caa8ad2d55cf78a855
Seth Eaton <setheaton@Seths-Air.lan>
2026-06-08 11:41:55 -0400

    Updates to values_table() function

--- R/format.R
+++ R/format.R
@@ -1,31 +1,39 @@
 #' Generates table of expected values (STILL IN DEVELOPMENT)
 #' @export
-values_table <- function(draws1, draws2, draws3, codes) {
+values_table <- function(draws, outs, components, codes) {
   # take a vector of bayesian models as a parameter
   # take a vector of component column names
 
-  outcomes <- get_outcomes_asframe(draws1, codes)
-  colnames(outcomes) <- c("A", "B", "C", "D", "E", "out1")
-  outcomes$out2 <- get_outcomes_asframe(draws2, codes)$out
-  outcomes$out3 <- get_outcomes_asframe(draws3, codes)$out
+  # get number of components
+  k <- length(components)
 
-  outcomes <- scale_outcome(outcomes, "out1", "out1.scale")
-  outcomes <- scale_outcome(outcomes, "out2", "out2.scale")
-  outcomes <- scale_outcome(outcomes, "out3", "out3.scale")
+  # set up outcomes frame
+  outcomes <- get_outcomes_asframe(draws[[1]], codes)
+  colnames(outcomes) <- append(components, outs[1])
 
-  value.summary <- codes[,2:6]
-  value.summary <- get_alternatives_names(value.summary)
+  if (length(draws) > 1) {
+    for (i in 2:length(draws)) {
+      outcomes$tmp <- get_outcomes_asframe(draws[[i]], codes)$out
+      colnames(outcomes) <- append(head(colnames(outcomes), -1), outs[i])
+    }
+  }
+
+  for (i in 1:length(outs)) {
+    outcomes <- scale_outcome(outcomes, outs[i], paste0(outs[i], ".scale"))
+  }
+
+  value.summary <- codes[,2:(2 + k - 1)]
+  value.summary <- get_alternatives_names(value.summary, components)
 
   # iterate through alternatives
-  for (i in 1:32) {
-    # get subset of outcomes for that alternative
-    subset = match_df(outcomes, value.summary[i,1:5])
-    value.summary$out1[i] = mean(subset$out1)
-    value.summary$out2[i] = mean(subset$out2)
-    value.summary$out3[i] = mean(subset$out3)
-    value.summary$out1.scale[i] = mean(subset$out1.scale)
-    value.summary$out2.scale[i] = mean(subset$out2.scale)
-    value.summary$out3.scale[i] = mean(subset$out3.scale)
+  for (i in 1:2^k) {
+    for (j in 1:length(outs)){
+      # get subset of outcomes for that alternative
+      subset <- match_df(outcomes, value.summary[i,1:length(components)])
+      value.summary[i, outs[j]] <- mean(subset[[outs[j]]])
+      scale <- paste0(outs[j], ".scale")
+      value.summary[i, scale] <- mean(subset[[scale]])
+    }
   }
 
   return(value.summary)
@@ -37,42 +45,37 @@ get_outcomes_asframe <- function(draws, codes) {
   # For a given draw index i, extract draw i from the posterior as a matrix and
   # left-multiply by codes to produce the outcome matrix for that draw
   out.list <- lapply(unique(draws$.draw), function(i) {
-    draw_matrix <- draws |> filter(.draw == i) |> select(4:35) |> as.matrix()
+    draw_matrix <- draws |> filter(.draw == i) |> select(4:length(draws)) |> as.matrix()
     as.matrix(codes) %*% t(draw_matrix)
   })
 
   # add outcomes data to a frame with effects codes and return
+  # To-DO: MAKE THIS RESPONSIVE TO VARIOUS NUMBERS OF COMPONENTS
   outcomes = data.frame(A = rep(c(codes$A), 4000),
                         B = rep(c(codes$B), 4000),
                         C = rep(c(codes$C), 4000),
                         D = rep(c(codes$D), 4000),
-                        E = rep(c(codes$E), 4000),
+                        #E = rep(c(codes$E), 4000),
                         out = unlist(out.list))
   return(outcomes)
 }
 
 # helper function to add intervention names to codes frame for k = 5
-get_alternatives_names <- function(value.summary) {
+get_alternatives_names <- function(value.summary, components) {
+  # get number of components
+  k <- length(components)
+
+  # initialize names vector
   names.vec <- c()
-  for (i in 1:32) {
+  for (i in 1:2^k) {
     string <- ""
-    if (sum(value.summary[i, 1:5]) == -5) {
+    if (sum(value.summary[i, 1:k]) == -1*k) {
       string = "All Off"
     } else {
-      if (value.summary[i, 1] == 1) {
-        string <- paste0(string, "A")
-      }
-      if (value.summary[i, 2] == 1) {
-        string <- paste0(string, "B")
-      }
-      if (value.summary[i, 3] == 1) {
-        string <- paste0(string, "C")
-      }
-      if (value.summary[i, 4] == 1) {
-        string <- paste0(string, "D")
-      }
-      if (value.summary[i, 5] == 1) {
-        string <- paste0(string, "E")
+      for (j in 1:k){
+        if(value.summary[i, j] == 1) {
+          string <- paste0(string, components[j])
+        }
       }
     }
     names.vec <- append(names.vec, string)
